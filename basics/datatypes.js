71b4a4d4cbdcecb58eab27dea3ceb750267f6938
jangirp22 <137917826+jangirp22@users.noreply.github.com>
2025-02-02 06:57:42 +0000

    data

---
+++ basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; 
+/* treat all JS codes as newer version. 
+If not written then also it runs the newer version only. 
+But it is machine dependent so prefered to write it. */
+
+// "https://archives.ecma-international.org/1996/TC39/96-002.pdf" for ECMA standards.
+// "mdn" can also be used.
+
+let age = 20
+let name = "Purav"
+let girlfriend = null
+let cpi;
+
+// symbol -> unique values (a Datatype)
+
+console.log(typeof null); // object
+console.log(typeof girfriend); // undefined -> Since, the value is null so it could be any from number, sring, boolean, etc.
+console.log(typeof cpi) // undefined
---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+console.log("Purav")
+
+const accountID = 3
+let accoutEmail = 'jangirpoorab@gmail.com'
+var accoutPassword = '12345678' // var should not be used because it has scope issues.
+accountCity = 'Jaipur'
+let accountState;
+
+console.log(accountID)
+console.table([accountID, accoutEmail, accoutPassword, accountCity])
